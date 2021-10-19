67ed7d95bdc30e0f90700c87f4b20423508d694b
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-19 22:11:36 +0300

    task88

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.text.DateFormatSymbols;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.nextLine());
+        Task task = new Task(n);
+        task.checkIf3();
+        task.reversedLine();
+        task.changeFirsAndLastItem();
+        task.add1();
+    }
+    }
