aa9300e82b62b2a7393b30051a3659b4142b42c6
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-24 14:56:07 +0300

    implemented CallsDuration task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        
+    Scanner in = new Scanner(System.in);
+        System.out.println("Input county 1, county 2 and county 3");
+        String c1 = in.nextLine();
+        String c2 = in.nextLine();
+        String c3 = in.nextLine();
+        System.out.println("Input duration of talks");
+        int t1 = in.nextInt();
+        int t2 = in.nextInt();
+        int t3 = in.nextInt();
+
+        System.out.printf("Call from %s lasts %d minutes; \n" +
+                "Call from %s lasts %d minutes; \n" +
+                "Call from %s lasts %d minutes; \n", c1, t1, c2, t2, c3, t3);
+        System.out.printf("All talk together last : %d minutes", t1+t2+t3);
     }
-    }
+}
