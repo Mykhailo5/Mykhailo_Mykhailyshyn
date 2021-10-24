3f0b206eb58370bb1584bdc9cb12fbd86fac780c
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-24 14:59:12 +0300

    implemented flowerBed task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        double radius = 8.5;
+        double perimeter = 2 * Math.PI * radius;
+        System.out.println("Perimeter is = " + perimeter);
+        double area = Math.PI * radius * radius;
+        System.out.println("Area is = " + area);
     }
     }
