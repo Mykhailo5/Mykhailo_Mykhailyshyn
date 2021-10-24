aadcdf2eafa49315b3dcf20695648902ac18c212
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-24 17:42:22 +0300

    implemented task 5.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        readFloats(4.15f, -4.66f, 4.59f);
+        System.out.println(readMinIntegers(new int[]{ 15, 7, 6, 18}));
+        System.out.println(readMaxIntegers(new int[]{3, 56, 34, 76, 8}));
+        HTTPerrors(300);
+    }
+
+    public static void readFloats(float a, float b, float c){
+    if((a<=-5 || a>=5) || (b<=-5 || b>=5) || (c<=-5 || c>=5)){
+        System.out.println("Number is below -5 or higher than 5");
+    }
+    else {
+        System.out.println("First number: " + a + " Second number: " + b + " Third number: " + c);
     }
     }
+
+    public static int readMinIntegers(int[] arr){
+    int rez = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(rez>arr[i]){
+                rez = arr[i];
+            }
+        }
+        return rez;
+    }
+
+    public static int readMaxIntegers(int[] arr){
+        int rez = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(rez<arr[i]){
+                rez = arr[i];
+            }
+        }
+        return rez;
+    }
+
+    public static void HTTPerrors(int err){
+    if(err==400){
+        System.out.println("Поганий запит");
+    }
+    else if(err==401){
+        System.out.println("Неавторизовано");
+    }
+    else if(err==402){
+        System.out.println("Необхідна оплата");
+    }
+    else{
+        System.out.println("Not an error");
+    }
+    }
+
+}
