2e9402b79808f2d49c8e039c75564162f1085990
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-29 12:03:12 +0300

    implemented task 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        Bird[] b = new Bird[3];
+        b[0]=new Eagle();
+        b[1]=new Swallow();
+        b[2]=new Penguin();
+
+        b[0].fly();
+        System.out.println(b[0].feathers);
+        System.out.println(b[0].layEggs);
+        System.out.println("-----------------");
+        b[1].fly();
+        System.out.println(b[1].feathers);
+        System.out.println(b[1].layEggs);
+        System.out.println("-----------------");
+        b[2].fly();
+        System.out.println(b[2].feathers);
+        System.out.println(b[2].layEggs);
     }
-    }
+}
