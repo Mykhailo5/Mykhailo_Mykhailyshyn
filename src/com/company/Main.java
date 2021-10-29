ee76b75299cae010877841b4ad17a21995225a25
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-29 15:14:53 +0300

    implemented task 10.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,26 @@ package com.company;
 import java.text.DateFormatSymbols;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        Map<String, String> personMap = new HashMap<String, String>();
+        personMap.put("Ivanov", "Oleg");
+        personMap.put("Petrov", "Mykola");
+        personMap.put("Mykhailyshyn", "Mykhailo");
+        personMap.put("Vasylyshyn", "Yaroslav");
+        personMap.put("Merza", "Maria");
+        personMap.put("Levitskiy", "Oleg");
+        personMap.put("Pelenskiy", "Arthur");
+        personMap.put("Popovych", "Mykhailo");
+        personMap.put("Seniuta", "Denys");
+        personMap.put("Palyvoda", "Maryana");
+
+        System.out.println("personMap before removal: " + personMap);
+        personMap.values().removeIf(value -> value == "Mykhailo");
+        System.out.println("personMap after removal: " + personMap);
+
     }
     }
