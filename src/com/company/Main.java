6b8068f66c638d9dfc7f5fd63c422411e384a458
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-29 15:06:52 +0300

    implemented task 10.2 personMap

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,35 @@ package com.company;
 import java.text.DateFormatSymbols;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        Map<String, String> personMap = new HashMap<String, String>();
+        personMap.put("Ivanov", "Oleg");
+        personMap.put("Petrov", "Mykola");
+        personMap.put("Mykhailyshyn", "Mykhailo");
+        personMap.put("Vasylyshyn", "Yaroslav");
+        personMap.put("Merza", "Maria");
+        personMap.put("Levitskiy", "Oleg");
+        personMap.put("Pelenskiy", "Arthur");
+        personMap.put("Popovych", "Mykhailo");
+        personMap.put("Seniuta", "Denys");
+        personMap.put("Palyvoda", "Maryana");
+
+        //to find by value
+        System.out.println("personMap before removal: " + personMap);
+        personMap.values().removeIf(value -> value == "Mykhailo");
+        System.out.println("personMap after removal: " + personMap);
+
+        //to find by key
+      /*for(Iterator<String> iterator = personMap.keySet().iterator(); iterator.hasNext(); ) {
+            String key = iterator.next();
+            if(key == "Mykhailo") {
+                iterator.remove();
+            }
+        }
+        System.out.println(personMap);*/
     }
     }
