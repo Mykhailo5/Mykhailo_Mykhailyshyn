eea2ac59786ca3eded37f82aa8d28718277c74dd
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-10-29 14:55:03 +0300

    implemented task 9 div

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,26 @@ import java.util.Date;
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        System.out.println(div(4.5, 6.6));
+        System.out.println(readNumber(5,10));
+    }
+
+    public static double div(double a, double b) throws Exception {
+        if(a==0 || b==0) throw new Exception("Can't divide by 0");
+            return a/b;
+    }
+
+
+    public static int readNumber(int start, int end) throws Exception {
         Scanner in = new Scanner(System.in);
-        int n = Integer.parseInt(in.nextLine());
-        Task task = new Task(n);
-        task.checkIf3();
-        task.reversedLine();
-        task.changeFirsAndLastItem();
-        task.add1();
+        System.out.println("Input number: ");
+        int num = in.nextInt();
+        if(num<start || num>end){
+            throw new Exception("Entered number is out of bounds");
+        }
+        else {
+            return num;
+        }
     }
     }
